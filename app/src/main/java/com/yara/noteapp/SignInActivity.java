7b3d4fa79c69b28e826d1870c21ff672bfb33865
yaraesam <yaraesam1997@gmail.com>
2019-12-25 08:49:03 +0200

    initial Commit

--- app/src/main/java/com/yara/noteapp/SignInActivity.java
+++ app/src/main/java/com/yara/noteapp/SignInActivity.java
@@ -22,8 +22,8 @@ public class SignInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-                startActivity(intent);
+              /*  Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(intent);*/
             }
         });
     }
