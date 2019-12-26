603cf5cc631b1ec325a45b9877960d3e55786cd4
Anwar <anwar.dev.96@gmail.com>
2019-12-26 12:08:30 +0200

    Add Note Get Notes from Firebase

--- app/src/main/java/com/yara/noteapp/NoteActivity.java
+++ app/src/main/java/com/yara/noteapp/NoteActivity.java
@@ -1,8 +1,11 @@
 package com.yara.noteapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class NoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+
+        ImageView img_back = findViewById(R.id.img_back);
+        img_back.setOnClickListener(view -> onBackPressed());
+
+        TextView note_name = findViewById(R.id.note_name);
+        TextView note_desc = findViewById(R.id.note_desc);
+        TextView note_date = findViewById(R.id.note_date);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String desc = intent.getStringExtra("desc");
+        long createdAt = intent.getLongExtra("createdAt", 0);
+
+        note_name.setText(title);
+        note_date.setText(createdAt + "");
+        note_desc.setText(desc);
+
+
     }
 }
--- app/src/main/java/com/yara/noteapp/NotesActivity.java
+++ app/src/main/java/com/yara/noteapp/NotesActivity.java
@@ -1,41 +1,114 @@
 package com.yara.noteapp;
 
 import android.app.Dialog;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.yara.noteapp.adapter.NoteAdapter;
+import com.yara.noteapp.model.Note;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class NotesActivity extends AppCompatActivity {
 
-    FloatingActionButton fab;
+    final String Uid = SplashActivity.getUid();
+    DatabaseReference noteReference;
+    String notebookId;
+
+    RecyclerView notesList_rv;
+    NoteAdapter noteAdapter;
+    List<Note> noteList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
-        initViews();
 
+        notebookId = getIntent().getStringExtra("notebook_id");
+        noteReference = FirebaseDatabase.getInstance().getReference().child("Note");
+        initViews();
+        initData();
     }
 
     private void initViews() {
-        fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showAddDialog();
-            }
-        });
+        notesList_rv = findViewById(R.id.notesList_rv);
+        notesList_rv.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(this, noteList);
+        notesList_rv.setAdapter(noteAdapter);
+
+        ImageView img_back = findViewById(R.id.img_back);
+        img_back.setOnClickListener(view -> onBackPressed());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> showAddDialog());
     }
 
+    private void initData() {
+        Query query;
+        if (notebookId == null)
+            query = noteReference.orderByChild("uid").equalTo(Uid);
+        else
+            query = noteReference.orderByChild("notebookId").equalTo(notebookId);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                noteList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    Note note = snapshot.getValue(Note.class);
+                    noteList.add(note);
+
+                }
+                noteAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+       /* noteReference
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        noteList.clear();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                            Note note = snapshot.getValue(Note.class);
+                            noteList.add(note);
+
+                        }
+                        notebookAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+*/
+    }
 
     private void showAddDialog() {
         final Dialog dialog = new Dialog(this);
@@ -43,11 +116,16 @@ public class NotesActivity extends AppCompatActivity {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.layout_add_note);
         TextView save = dialog.findViewById(R.id.save);
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
+        final EditText title = dialog.findViewById(R.id.title);
+        final EditText description = dialog.findViewById(R.id.description);
+        save.setOnClickListener(v -> {
+            dialog.dismiss();
+            String key = noteReference.push().getKey();
+            Note note = new Note(key, Uid, notebookId, title.getText().toString()
+                    , description.getText().toString(), new Date().getTime());
+            noteReference.child(key).setValue(note.toMap());
+            noteList.add(note);
+            noteAdapter.notifyDataSetChanged();
         });
         dialog.show();
     }
